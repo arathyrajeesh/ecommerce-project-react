a634a5aac84be03da9bf59e23d715be43eee2677
Arathy <arathyrajeesh2@gmail.com>
2025-08-17 19:57:44 +0530

    style chnages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import '../styles/hero.css';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import "../styles/hero.css";
+import { Link } from "react-router-dom";
 
 function Hero() {
     return (
         <main className="hero-section">
+        <img
+            src="https://static.wixstatic.com/media/82fcd3_2740861c485d420c865a8738236a50d9~mv2_d_4804_3203_s_4_2.jpg/v1/fill/w_758,h_755,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/82fcd3_2740861c485d420c865a8738236a50d9~mv2_d_4804_3203_s_4_2.jpg"
+            alt="Nature"
+            className="hero-image"
+        />
+
         <div className="hero-content">
-            <h1>BECOME <br/> ONE WITH <br /> NATURE</h1>
+            <h1>
+            BECOME <br /> ONE WITH <br /> NATURE
+            </h1>
             <p>Everything You Need for the Perfect <br /> Travel Experience</p>
-            <a className='cta-button'><Link to="/shop">Shop Our Range</Link></a>
+            <Link to="/shop" className="cta-button">
+            Shop Our Range
+            </Link>
         </div>
         </main>
     );
