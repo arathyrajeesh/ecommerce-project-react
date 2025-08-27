d6ef25f637030a8230eff7edbcabc793b5718c42
Arathy <arathyrajeesh2@gmail.com>
2025-08-27 20:52:51 +0530

    graph added

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -21,218 +21,219 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
-import SettingsIcon from '@mui/icons-material/Settings';
 import PeopleIcon from '@mui/icons-material/People';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import CategoryIcon from '@mui/icons-material/Category';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PaymentIcon from '@mui/icons-material/Payment';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import ChartBar from './ChartBar';
+import Donut from '../components/DonutGraph'
+
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
 });
 
 const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+    shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
+    zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
     }),
-  }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: 'nowrap',
-  boxSizing: 'border-box',
-  ...(open && {
-    ...openedMixin(theme),
-    '& .MuiDrawer-paper': openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    '& .MuiDrawer-paper': closedMixin(theme),
-  }),
+    shouldForwardProp: (prop) => prop !== 'open',
+    })(({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+    }),
 }));
 
 const items = [
-  { text: 'Orders', icon: <CategoryIcon /> },
-  { text: 'Products', icon: <ShoppingCartIcon /> },
-  { text: 'Customers', icon: <PeopleIcon /> },
-  { text: 'Collections', icon: <CollectionsIcon /> },
+    { text: 'Orders', icon: <CategoryIcon /> },
+    { text: 'Products', icon: <ShoppingCartIcon /> },
+    { text: 'Customers', icon: <PeopleIcon /> },
+    { text: 'Collections', icon: <CollectionsIcon /> },
 ];
 
-const listItems = [
-  { text: 'Analytics', icon: <TrendingUpIcon /> },
-  { text: 'Settings', icon: <SettingsIcon /> },
-  { text: 'Return/Refunds', icon: <PaymentIcon /> },
-];
 
 export default function Dashboard() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
 
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
+return (
+<Box sx={{ display: 'flex' }}>
+    <CssBaseline />
+    <AppBar position="fixed" open={open}>
         <Toolbar>
-          <IconButton
+            <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
             sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
+            marginRight: 5,
+            ...(open && { display: 'none' }),
             }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
             Dashboard
-          </Typography>
+            </Typography>
         </Toolbar>
-      </AppBar>
-
-      <Drawer variant="permanent" open={open}>
+    </AppBar>
+    <Drawer variant="permanent" open={open}>
         <DrawerHeader>
-          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
             MADAGASCAR
-          </Typography>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
+            </Typography>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
         </DrawerHeader>
-
         <Divider />
-
         <List>
-          {items.map((item) => (
-            <ListItem key={item.text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {item.icon}
-                </ListItemIcon>
-                <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+            {items.map((item) => (
+                <ListItem key={item.text} disablePadding sx={{ display: 'block' }}>
+                    <ListItemButton
+                    sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
+                    }}
+                    >
+                        <ListItemIcon
+                        sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : 'auto',
+                        justifyContent: 'center',
+                    }}
+                    >
+                        {item.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
         </List>
-      </Drawer>
-
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+    </Drawer>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader /> 
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={8}>   
-            <Card sx={{ minHeight: 150, boxShadow: 3 }}>
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  Total Orders
-                </Typography>
-                <Typography variant="h4" color="primary">
-                  1,245
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">View Orders</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} md={4}>   
-            <Card sx={{ minHeight: 150, boxShadow: 3 }}>
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  Total Products
-                </Typography>
-                <Typography variant="h4" color="secondary">
-                  356
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Manage Products</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} md={12}>   
-            <Card sx={{ minHeight: 150, boxShadow: 3 }}>
-              <CardContent>
-                <Typography variant="h6" gutterBottom>
-                  Customers
-                </Typography>
-                <Typography variant="h4" color="success.main">
-                  890
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">View Customers</Button>
-              </CardActions>
-            </Card>
-          </Grid>
+        <Grid container spacing={1}>
+            <Grid item xs={2} md={4} size={4}>
+                <Card sx={{minHeight: 100,boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography variant="h6" gutterBottom> Orders</Typography>
+                        <Typography variant="h4" sx={{ color: 'primary.main' }}>
+                        245
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View Orders</Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={3} md={4} size={4}>
+                <Card sx={{ minHeight: 100,boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography variant="h6" gutterBottom> Products</Typography>
+                        <Typography variant="h4" sx={{ color: 'primary.main' }}>
+                        245
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View Orders</Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={3} md={4} size={4}>
+                <Card sx={{minHeight: 100, boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography variant="h6" gutterBottom>Payments</Typography>
+                        <Typography variant="h4" sx={{ color: 'primary.main' }}>
+                        1,245
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">View Orders</Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+            <Grid item xs={2} md={4} size={6}>
+                <Card sx={{ minHeight: 150, boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography variant="h5" sx={{ color: 'black' }}>Profit</Typography>
+                        <Typography variant="p" sx={{ color: 'grey' }}>Last Month</Typography>
+                        <ChartBar/>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={2} md={4} size={6}>
+                <Card sx={{ minHeight: 10, boxShadow: 3 }}>
+                    <CardContent>
+                        <Typography variant="h5" sx={{ color: 'black' }}>Report</Typography>
+                        <Typography variant="p" sx={{ color: 'grey' }}>Monthly Report</Typography>
+                        <Donut/>
+                    </CardContent>
+                </Card>
+            </Grid>
         </Grid>
-      </Box>
     </Box>
-  );
+</Box>
+);
 }
